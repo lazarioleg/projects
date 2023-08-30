138073abbcca38a38139e197e5352b77763aadc0
Oleg <lazarioleg97@gmail.com>
2023-08-30 19:38:14 -0400

    data contains outliers

--- MLProjects/Stock Price Prediction.py
+++ MLProjects/Stock Price Prediction.py
@@ -14,6 +14,7 @@ from sklearn import metrics
 import warnings
 warnings.filterwarnings("ignore")
 
+#explains distribution and transfers to data for usage
 def dataBaseExplanation(filename):
     df = pd.read_csv(filename)
     df.head()
@@ -21,10 +22,50 @@ def dataBaseExplanation(filename):
     data_analysis = df.describe()
     data_analysis.to_csv('output.csv', index=False)
     os.replace('output.csv', 'MLProjects\data\\data.csv')
+    df.info()
+
+#returns the rows of data
+def returnDataFrame(filename):
+    df = pd.read_csv(filename)
+    df.head()
+    return df
+
+def dataAnalysisFDA(df):
+    plt.figure(figsize=(15,8))
+    plt.plot(df['Close'])
+    plt.title('Tesla Closing Price', fontsize=15)
+    plt.ylabel('Price in $USD')
+    plt.xlabel('Days from 2010')
+    plt.show()
+
+def distributionPlotShower(df):
+    print(df.head())
+    df[df['Close'] == df['Adj Close']].shape
+    df = df.drop(['Adj Close'], axis=1)
+    print("\n")
+    print('Null Checking (Any num besides 0 is undefined)\n' )
+    print(df.isnull().sum())
+
+    filters = ['Open', 'High', 'Low', 'Close', 'Volume']
+
+    plt.subplots(figsize=(25,10))
+
+    for i, col in enumerate(filters):
+        plt.subplot(2,3,i+1)
+        sb.distplot(df[col])
+    plt.show()
+
+
+
 
 
 def main():
-    dataBaseExplanation('MLProjects\data\TSLA.csv')
+    test_data = 'MLProjects\data\TSLA.csv'
+    dataBaseExplanation(test_data)
+    print("\n")
+    #dataAnalysisFDA(returnDataFrame(test_data))
+    distributionPlotShower(returnDataFrame(test_data))
+
 
 if __name__ == "__main__":
     main()
