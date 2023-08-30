cb466187a20f04627be3bf5f45b30844b6c68e3d
Oleg <lazarioleg97@gmail.com>
2023-08-30 18:58:49 -0400

    initial commit

---
+++ Machine Learning Projects/Stock Price Prediction.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+from sklearn import *
+ 
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
+from sklearn import metrics
+ 
+import warnings
+
