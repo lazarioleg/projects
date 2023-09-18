e330e63013c8c3f28fe7ed6fa7032af5d6605ede
Oleg Lazari <111822913+lazarioleg@users.noreply.github.com>
2023-09-18 15:30:36 -0400

    Delete mud-game directory

--- mud-game/player.py
+++
@@ -1,7 +0,0 @@
-class Player:
-    def __init__(self, name, health, attack, defense, inventory):
-        self.name = name
-        self.health = health
-        self.attack = attack
-        self.defense = defense
-        self.inventory = inventory
