5a70e3459c8bd77462465ddc7e4edc17a33852e5
Oleg <lazarioleg97@gmail.com>
2024-02-16 13:57:31 -0500

    added newline checking and scrolling

--- OS_SYSTEM/kernel.c
+++ OS_SYSTEM/kernel.c
@@ -81,11 +81,26 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
 {
 	const size_t index = y * VGA_WIDTH + x;
 	terminal_buffer[index] = vga_entry(c, color);
+
+	memmove(terminal_buffer, terminal_buffer + VGA_WIDTH, VGA_WIDTH * (VGA_HEIGHT - 1) * sizeof(uint16_t));
+
+	size_t index = (VGA_HEIGHT - 1) * VGA_WIDTH;
+	for(size_t x = 0; x < VGA_WIDTH; ++x)
+	{
+		terminal_buffer[index + x] = vga_entry(' ', terminal_color);
+	}
 }
  
 void terminal_putchar(char c) 
 {
 	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+    
+    //newline check
+	if(c == '\n'){
+		terminal_row++;
+		terminal_column = 0;
+	}
+
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
 		if (++terminal_row == VGA_HEIGHT)
