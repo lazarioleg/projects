a9077a78287bdff4995dd045dde64d3b8a003c18
Oleg <lazarioleg97@gmail.com>
2024-02-19 10:21:30 -0500

    Update error message in kernel.c

--- QUICK_OS_SYSTEM/kernel.c
+++ QUICK_OS_SYSTEM/kernel.c
@@ -9,7 +9,7 @@
  
 /* This tutorial will only work for the 32-bit ix86 targets. */
 #if !defined(__i386__)
-#error "This tutorial needs to be compiled with a ix86-elf compiler"
+#error "This compiler is not a 32-bit x86 instance"
 #endif
  
 enum vga_color {
